dac8f17979d18fef63a508816fa6be61f000fee5
DmitrySuro <dmitrysurodeev@yandex.ru>
2022-10-04 22:47:42 +0300

    add second task

--- task2.py
+++ task2.py
@@ -1,2 +1,15 @@
-# Задайте натуральное число N. Напишите программу, 
+# Задайте натуральное число N. Напишите программу,
 # которая составит список простых множителей числа N.
+
+user_number = int(input('Insert your number: '))
+
+i = 2
+multipliers = []
+while i * i <= user_number:
+    while user_number % i == 0:
+        multipliers.append(i)
+        user_number = user_number / i
+    i = i + 1
+if user_number > 1:
+    multipliers.append(user_number)
+print(multipliers)
--- task3.py
+++ task3.py
@@ -1,2 +1,18 @@
 # Задайте последовательность чисел. Напишите программу, 
 # которая выведет список неповторяющихся элементов исходной последовательности.
+
+succession_digits = [23,1,4,23,2421,321,5867,34,97]
+uniq_digits = []
+for i in succession_digits:
+    elem = i
+    for j in succession_digits:
+        if elem == j:
+            uniq_digits.append(elem)
+            break
+
+print(uniq_digits)
+        
+
+
+
+    
