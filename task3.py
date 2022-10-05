8c9dce01d99f27adae44067c5f2d0fde65266a9a
DmitrySuro <dmitrysurodeev@yandex.ru>
2022-10-05 22:56:19 +0300

    add task 3

--- task3.py
+++ task3.py
@@ -1,18 +1,13 @@
-# Задайте последовательность чисел. Напишите программу, 
+# Задайте последовательность чисел. Напишите программу,
 # которая выведет список неповторяющихся элементов исходной последовательности.
 
-succession_digits = [23,1,4,23,2421,321,5867,34,97]
-uniq_digits = []
-for i in succession_digits:
-    elem = i
-    for j in succession_digits:
-        if elem == j:
-            uniq_digits.append(elem)
-            break
 
-print(uniq_digits)
-        
+succession_digits = [1, 2, 3546, 2, 5, 8, 4, 4, 4, 1, 8, 9, 99, 9]
 
+my_list = []
 
+for elem in succession_digits:
+    if succession_digits.count(elem) == 1:
+        my_list.append(elem)
 
-    
+print(my_list)
--- task4/task4.py
+++ task4/task4.py
@@ -3,3 +3,11 @@
 #   записать в файл многочлен степени k.
 # Пример:
 # - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+import random
+k = random.randint(0,101)
+
+
+
+data = open('File.txt', 'w')
+data.write('1')
+data.close()
