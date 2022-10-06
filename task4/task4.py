4b0645ebcdab382bc36c2d4f9589515fd9192690
DmitrySuro <dmitrysurodeev@yandex.ru>
2022-10-06 22:53:52 +0300

    Решена 4 щадача

--- task4/task4.py
+++ task4/task4.py
@@ -3,11 +3,12 @@
 #   записать в файл многочлен степени k.
 # Пример:
 # - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+
 import random
 k = random.randint(0,101)
+poly = '2*x**k + 4*x + 5 = 0'
+poly.replace('k', str(k))
 
-
-
-data = open('File.txt', 'w')
-data.write('1')
+data = open('File.txt', 'a')
+data.writelines(poly.replace('k', str(k)))
 data.close()
